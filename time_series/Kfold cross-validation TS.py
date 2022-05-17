f842c2452bd4ec289994877ba81dbbb3870ad404
Luis Guilherme Ribeiro <luis.ribeiro@trizy.com.br>
2022-05-17 10:02:34 -0300

    Studies

---
+++ time_series/Kfold cross-validation TS.py
@@ -0,0 +1,18 @@
+# Import TimeSeriesSplit
+from sklearn.model_selection import TimeSeriesSplit
+
+# Create TimeSeriesSplit object
+time_kfold = TimeSeriesSplit(n_splits=3)
+
+# Sort train data by date
+train = train.sort_values('date')
+
+# Iterate through each split
+fold = 0
+for train_index, test_index in time_kfold.split(train):
+    cv_train, cv_test = train.iloc[train_index], train.iloc[train_index]
+    
+    print('Fold :', fold)
+    print('Train date range: from {} to {}'.format(cv_train.date.min(), cv_train.date.max()))
+    print('Test date range: from {} to {}\n'.format(cv_test.date.min(), cv_test.date.max()))
+    fold += 1
